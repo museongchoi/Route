47d28ab39014df9f23209ea2169d818753d39bda
museongchoi <antjd3303@hs.ac.kr>
2026-05-12 18:18:01 +0900

    [chore] BackendServer 프로젝트 초기 생성

---
+++ Route_BackendServer/Route_BackendServer.cpp
@@ -0,0 +1,15 @@
+﻿// Route_BackendServer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "RouteBackendServer started." << std::endl;
+    std::cout << "Press Enter to exit..." << std::endl;
+
+    std::cin.get();
+    return 0;
+}
+
+
